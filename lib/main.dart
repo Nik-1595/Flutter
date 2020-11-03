90b3e17edb5c67c72b22cd0e97172ab128035d52
developers_b <developers_b@evenforce.com>
2020-11-03 10:40:43 +0530

    Buttons and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main(){
-    runApp(MyApp());
+    runApp(MaterialApp(
+
+      home: MyApp(),
+
+    ));
+
 }
 
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+
+      appBar: AppBar(
+
+        title: Text('My First App'),
+
+        centerTitle: true,
+
+        backgroundColor: Colors.redAccent,
+
+      ),
+
+      body: Center(
+        child:RaisedButton.icon(
+          onPressed: () {},
+          icon: Icon(
+            Icons.mail
+          ),
+          label: Text('mail me'),
+          color: Colors.amber,
+        ),
+      ),
 
 
-class MyApp extends StatefulWidget {
+      floatingActionButton: FloatingActionButton(
 
-    @override
-    State<StatefulWidget> createState(){
-      return MyAppState();
-    }
-}
+        onPressed: (){},
 
-class MyAppState extends State<MyApp>{
+        child: Text('click'),
 
+        backgroundColor: Colors.deepOrangeAccent,
 
-  var questionIndex = 0;
+      ),
 
-  void answer(){
-    setState(() {
-
-        questionIndex = questionIndex +1 ;
-
-
-    });
-
+    );
   }
-
-    @override
-    Widget build(BuildContext ctx){
-
-        var question=[
-          'What\'s your favourite animal',
-          'What\'s your favourite color',
-        ];
-
-          return MaterialApp(home: Scaffold(
-            appBar: AppBar(title: Text('My First App'),),
-            body: Column(
-              children: [
-                Text(
-                    question[questionIndex],
-                ),
-                RaisedButton(
-                    child: Text('Answer'),
-                    onPressed: answer),
-                RaisedButton(
-                    child: Text('Answer 1'),
-                    onPressed: answer),
-                RaisedButton(
-                    child: Text('Answer 2'),
-                    onPressed: answer)
-              ],
-            ),
-          ));
-    }
 }
+
+
+
+
