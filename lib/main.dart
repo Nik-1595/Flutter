40f97274846677ffb1113b055cf6e12c96bc7aca
developers_b <developers_b@evenforce.com>
2020-11-05 09:53:58 +0530

    New App

--- lib/main.dart
+++ lib/main.dart
@@ -13,57 +13,87 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-
-      appBar: AppBar(
-
-        title: Text('My First App'),
-
-        centerTitle: true,
-
-        backgroundColor: Colors.redAccent,
-
-      ),
-
-      body:
-
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('Ninja ID Card'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0,40.0, 30.0,0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(20.0),
-              color: Colors.deepOrange,
-              child: Text('One'),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/yu.jpg'),
+                radius: 50.0,
+              ),
             ),
-            Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.cyanAccent,
-              child: Text('two'),
+            Divider(
+              height: 60.0,
+              color: Colors.amberAccent,
             ),
-            Container(
-              padding: EdgeInsets.all(40.0),
-              color: Colors.green,
-              child: Text('three'),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 1.0,
+              ),
             ),
+            SizedBox(height: 10.0),
+            Text(
+              'Nikhil S',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 1.0,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'Level',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 1.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Z+',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 1.0,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'nikhils@gmail.com',
+                  style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            )
           ],
-        )
-      ,
-
-
-      floatingActionButton: FloatingActionButton(
-
-        onPressed: (){},
-
-        child: Text('click'),
-
-        backgroundColor: Colors.deepOrangeAccent,
-
+        ),
       ),
-
     );
   }
 }
-
 
 
 
